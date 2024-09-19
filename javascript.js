b3031807287e28e3dd78fbafd058f571cb47516e
Seamus Xubuntu <151066590+ShaymooseIII@users.noreply.github.com>
2024-09-19 14:38:38 -0700

    creation of the grid container and styling

---
+++ javascript.js
@@ -0,0 +1,14 @@
+//function to create grid default will be 16
+    // Loop to create the X by X grid
+
+        //individual id and class assignment
+
+// event listener for when mouse enters box
+
+    //changing of the box color 
+
+// adding button to top of screen
+    
+//Function for button click to give user prompt 
+
+    //take input and create new grid 
