41c9f2f482afb0c39f841d1ba4024214400745b4
Seamus Xubuntu <151066590+ShaymooseIII@users.noreply.github.com>
2024-09-19 16:00:00 -0700

    added creation of rows and boxes for grid and color change when hovering over boxes

--- javascript.js
+++ javascript.js
@@ -1,11 +1,34 @@
+//declaration of document elements
+const gridContainer = document.querySelector(".grid-container");
 //function to create grid default will be 16
+function createGrid(gridSize = 16){
     // Loop to create the X by X grid
-
+    for(let i = 0; i < gridSize; i++){
         //individual id and class assignment
+        let row = document.createElement("div");
+        row.classList.add("grid-row");
+        gridContainer.appendChild(row);
+        for(let j = 1; j <= gridSize; j++){
+            let box = document.createElement("div");
+            box.classList.add("box");
+            box.setAttribute("id", `box-${i+j}`);
+            row.appendChild(box);
+        }
+
+    }
+}
+
+createGrid();
+
+
 
 // event listener for when mouse enters box
-
+gridContainer.addEventListener("mouseover", (e) =>{
+    box = e.target;
+    console.log(box);
     //changing of the box color 
+    box.style.backgroundColor = "blue";
+})
 
 // adding button to top of screen
     
